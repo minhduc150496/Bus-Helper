e3987afedfd7cdcc9069247967c9ba6fdb2a1d73
LongPham <long.phamlp94@gmail.com>
2017-07-12 22:03:44 +0700

    change config

--- BusHelper/ClassLibrary1/Model1.Designer.cs
+++ BusHelper/ClassLibrary1/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\DaiHoc\2017 Summer\SAD\Group_Project\BusHelper\ClassLibrary1\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\GitHub\BusHelper-web\BusHelper\ClassLibrary1\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
